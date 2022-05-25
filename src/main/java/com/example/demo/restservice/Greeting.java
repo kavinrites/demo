a80eabf3c11939d22a171a9db5b60b6848dedf12
tekipanda <t3k1panda@gmail.com>
2022-05-25 10:46:11 -0700

    was getting the below error.
    This application has no explicit mapping for /error, so you are seeing this as a fallback
    
    Spring only scans classes below the main package

--- src/main/java/com/example/demo/restservice/Greeting.java
+++ src/main/java/com/example/demo/restservice/Greeting.java
@@ -1,4 +1,4 @@
-package com.example.restservice;
+package com.example.demo.restservice;
 
 public class Greeting {
 
--- src/main/java/com/example/demo/restservice/GreetingController.java
+++ src/main/java/com/example/demo/restservice/GreetingController.java
@@ -1,6 +1,8 @@
-package com.example.restservice;
+package com.example.demo.restservice;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
